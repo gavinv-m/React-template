22f45c1c2e1dbd137a5b9996f031e68e51920438
Gavin <gavinvm@protonmail.com>
2024-10-28 17:40:30 +0200

    feat: Add custom rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -22,17 +22,47 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import-newlines': importNewlines,
     },
+    extends: ['prettier'],
     rules: {
+      //  React-related rules
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+
+      // Vite-specific rules
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+
+      // Custom rules
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'never',
+          exports: 'never',
+          functions: 'never',
+          imports: 'never',
+          objects: 'never',
+          trailing: 'always',
+        },
+      ],
+      'import-newlines/enforce': ['error', 2],
+      'no-console': 'warn',
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'object-curly-newline': [
+        'error',
+        {
+          ExportDeclaration: { multiline: true, minProperties: 3 },
+          ImportDeclaration: 'always',
+          ObjectExpression: { minProperties: 2, multiline: true },
+          ObjectPattern: { multiline: true },
+        },
+      ],
     },
   },
-]
+];
